11835ffe8b1e4f29084732f9f1026b238f127b29
Pablo velasquez <pabvelas01@gmail.com>
2020-07-30 14:15:49 -0400

    Agrega estadisticas para llevar control de juego

--- src/battle/Battle.java
+++ src/battle/Battle.java
@@ -17,39 +17,52 @@ public class Battle {
     public static void main(String[] args) {
         // TODO code application logic here
         int N=10;
+        int NUMERO_JUEGOS=100000;
+        Estadisticas e=new Estadisticas();
         Tablero t=new Tablero(N);
-        TableroRecorrido tableroAux=new TableroRecorrido(N);
-        BarcoGenerico portaAviones= new BarcoGenerico(5,'A');
-        BarcoGenerico buque= new BarcoGenerico(4,'B');
-        BarcoGenerico submarino= new BarcoGenerico(3,'S');
-        BarcoGenerico crucero= new BarcoGenerico(3,'C');
-        BarcoGenerico destructor= new BarcoGenerico(2,'C');
-        t.Imprimir();
-        System.out.println("/****************/");
-        for(int i=0;i<100;i++){
-            Posicion p=tableroAux.getNuevaPosicion();
-            
-            char resultado=t.disparo(p.getX()+1, p.getY()+1);
-            tableroAux.setPosicionVisitada(p.getX(), p.getY(),resultado);
-            switch(resultado){
-                case 'A': portaAviones.setPosicion(p.getX(), p.getY());
-                           break;
-                case 'B': buque.setPosicion(p.getX(), p.getY());
-                            break;
-                case 'S': submarino.setPosicion(p.getX(), p.getY());
-                            break;
-                case 'C': crucero.setPosicion(p.getX(), p.getY());
-                            break;
-                case 'D': destructor.setPosicion(p.getX(), p.getY());
-                            break;
-            }
-            
-            System.out.println(t.ganar());
-        }
+        TableroRecorrido tableroAux;
+        BarcoGenerico portaAviones;
+        BarcoGenerico buque;
+        BarcoGenerico submarino;
+        BarcoGenerico crucero;
+        BarcoGenerico destructor;
         
-        
-        t.Imprimir();
-        tableroAux.Imprimir();
+        for (int h=0;h<NUMERO_JUEGOS;h++){ /**Inicio de calculos de los 100.000 jugadas**/
+            t=new Tablero(N);
+             tableroAux=new TableroRecorrido(N);
+             portaAviones= new BarcoGenerico(5,'A');
+             buque= new BarcoGenerico(4,'B');
+             submarino= new BarcoGenerico(3,'S');
+             crucero= new BarcoGenerico(3,'C');
+             destructor= new BarcoGenerico(2,'C');
+           for(int i=0;i<100;i++){  /**Inicio de calculos de las 100 casillas 10*10**/
+               Posicion p=tableroAux.getNuevaPosicion();
+
+               char resultado=t.disparo(p.getX()+1, p.getY()+1);
+               tableroAux.setPosicionVisitada(p.getX(), p.getY(),resultado);
+               switch(resultado){
+                   case 'A': portaAviones.setPosicion(p.getX(), p.getY());
+                              break;
+                   case 'B': buque.setPosicion(p.getX(), p.getY());
+                               break;
+                   case 'S': submarino.setPosicion(p.getX(), p.getY());
+                               break;
+                   case 'C': crucero.setPosicion(p.getX(), p.getY());
+                               break;
+                   case 'D': destructor.setPosicion(p.getX(), p.getY());
+                               break;
+               }
+
+               int ganar=t.ganar();
+               if(ganar!=0){
+                   e.setResultadoFinalTablero(ganar);
+                   System.out.println(ganar);
+                   break;
+               }
+           }/**Fin de calculos de las 100 casillas 10*10**/
+           
+        } /**termino de calculos de los 100.000 jugadas**/
+        e.Impresion();
     }
     
 }
